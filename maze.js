8a459a64ab406bc5eac03d61cbb098e22980c788
Wonakee Amos <mulamos@Wonakees-MacBook.local>
2016-10-14 11:08:51 -0500

    youlose class add to all class boundary elements

--- maze.js
+++ maze.js
@@ -1,8 +1,18 @@
 window.onload = function(){
-	var topLeft = document.getElementById("boundary1");
-	topLeft.onmouseover=function (){
-		this.setAttribute("class", "boundary youlose");
-	}
+	var win = true;
+	var myBoundary = document.querySelectorAll(".boundary");
+	//apply event handler to all .boundary class
+		for (var i = 0; i < myBoundary.length; i++ )
+		{
+			myBoundary[i].addEventListener("mouseover", function() {
+				for (var j = 0; j < myBoundary.length; i++)
+				{
+					myBoundary[j].setAttribute("class", "boundary youlose");
+					//win = false;
+					//document.getElementById("status");
+				}
+				
+			});
+		}
+		
 };
-
-
