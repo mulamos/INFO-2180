0452190748c1d5ca712e79c7e63068bb2ce74aeb
Wonakee Amos <mulamos@Wonakees-MacBook.local>
2016-10-14 12:24:03 -0500

    added you win alert when user gets to the end without touching boundary

--- maze.js
+++ maze.js
@@ -1,11 +1,24 @@
-window.onload = function(){
+window.addEventListener("load", function(){
+	var win = true;
 	var myBoundary = document.querySelectorAll(".boundary");
+	var end = document.getElementById("end");
 	//apply event handler to all .boundary class
-		for (var i = 0; i < myBoundary.length; i++ )
+		for (var i = 0; i < myBoundary.length - 1; i++ )
 		{
 			myBoundary[i].addEventListener("mouseover", function() {
-				this.setAttribute("class", "boundary youlose");
-			})
+				for (var j = 0; j < myBoundary.length - 1; j++ )
+				{
+					myBoundary[j].setAttribute("class", "boundary youlose");
+					win = false;
+				}
+			});
 		}
-		
-};
+
+
+		end.addEventListener("mouseover", function(){
+			if (win){
+				alert("You Win!");
+			}
+			
+		})
+});
